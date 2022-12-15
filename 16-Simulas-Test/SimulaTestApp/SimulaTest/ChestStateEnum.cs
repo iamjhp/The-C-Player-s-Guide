5ee76b2dd2938e90d03f3cd8f4331e9be4f97aa3
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-15 20:44:07 +0100

    Finish Simula's Test

---
+++ 16-Simulas-Test/SimulaTestApp/SimulaTest/ChestStateEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulaTest {
+    public enum ChestStateEnum {
+        Open,
+        Closed,
+        Locked
+    }
+}
---
+++ 16-Simulas-Test/SimulaTestApp/SimulaTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimulaTest;
+
+ChestStateEnum currState = ChestStateEnum.Locked;
+
+while (true) {
+    Console.Write($"The chest is {currState.ToString().ToLower()}. What do you want to do? ");
+
+    string userInput = Console.ReadLine();
+
+    if (currState == ChestStateEnum.Open && userInput == "close") currState = ChestStateEnum.Closed;
+    if (currState == ChestStateEnum.Closed && userInput == "lock") currState = ChestStateEnum.Locked;
+    if (currState == ChestStateEnum.Closed && userInput == "open") currState = ChestStateEnum.Open;
+    if (currState == ChestStateEnum.Locked && userInput == "unlock") currState = ChestStateEnum.Closed;
+}
