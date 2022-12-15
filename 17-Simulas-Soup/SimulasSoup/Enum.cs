a1847cd11e34177bd300e64300e6463e0711d099
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-15 22:10:52 +0100

    Finish 17 Simula's Soup

---
+++ 17-Simulas-Soup/SimulasSoup/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulasSoup {
+    public enum SoupType {
+        Soup,
+        Stew,
+        Gumbo
+    }
+
+    public enum MainIngredient {
+        Mushroom,
+        Chicken,
+        Carrot,
+        Potato
+    }
+
+    public enum Seasoning {
+        Spicy,
+        Salty,
+        Sweet
+    }
+}
---
+++ 17-Simulas-Soup/SimulasSoup/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SimulasSoup;
+
+SoupType type = SoupType.Soup;
+
+(SoupType soupTye, MainIngredient mainIngredient, Seasoning seasoning) soup = (GetSoupType(), GetMainIngredient(), GetSeasoning());
+Console.WriteLine($"{soup.seasoning} {soup.mainIngredient} {soup.soupTye}");
+
+
+SoupType GetSoupType() {
+    Console.Write("Choose soup type (soup, stew, gumbo): ");
+    string input = Console.ReadLine();
+
+    return input switch {
+        "soup" => SoupType.Soup,
+        "stew" => SoupType.Stew,
+        "gumbo" => SoupType.Gumbo
+    };
+}
+
+MainIngredient GetMainIngredient() {
+    Console.Write("Choose main ingredient (mushroom, chicken, carrot, potato): ");
+    string input = Console.ReadLine();
+
+    return input switch {
+        "mushroom" => MainIngredient.Mushroom,
+        "chicken" => MainIngredient.Chicken,
+        "carrot" => MainIngredient.Carrot,
+        "potato" => MainIngredient.Potato
+    };
+}
+
+Seasoning GetSeasoning() {
+    Console.Write("Choose seasoning (spicy, salty, sweet): ");
+    string input = Console.ReadLine();
+
+    return input switch {
+        "spicy" => Seasoning.Spicy,
+        "salty" => Seasoning.Salty,
+        "sweet" => Seasoning.Sweet
+    };
+}
