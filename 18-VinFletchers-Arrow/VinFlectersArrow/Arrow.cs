801d6e357905bc8da83e9e04f4d5b41813ffb4fa
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-17 19:07:11 +0100

    Finish 18 Vin Fletcher's Arrow

---
+++ 18-VinFletchers-Arrow/VinFlectersArrow/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinFlectersArrow {
+    public class Arrow {
+        private ArrowHead arrowHead;
+        private Fletching fletching;
+        private float length;
+
+        public Arrow(ArrowHead arrowHead, Fletching fletching, float length) {
+            this.arrowHead = arrowHead;
+            this.fletching = fletching;
+            this.length = length;
+        }
+
+        public float GetCost() {
+            float arrowHeadCost = arrowHead switch {
+                ArrowHead.Steel => 10,
+                ArrowHead.Wood => 3,
+                ArrowHead.Obsidian => 5
+            };
+
+            float fletchingCost = fletching switch {
+                Fletching.Plastic => 10,
+                Fletching.GooseFeathers => 3,
+                Fletching.TurkeyFeathers => 5
+            };
+
+            float shaftCost = 0.05f * length;
+
+            return arrowHeadCost + fletchingCost + shaftCost;
+        }
+    }
+}
---
+++ 18-VinFletchers-Arrow/VinFlectersArrow/Program.cs
@@ -0,0 +1,46 @@
+﻿using VinFlectersArrow;
+
+Arrow arrow = GetArrow();
+Console.WriteLine($"That arrow costs {arrow.GetCost()} gold.");
+
+Arrow GetArrow() {
+    ArrowHead arrowHead = GetArrowHeadType();
+    Fletching flecthing = GetFletchingType();
+    float length = GetLength();
+
+    return new Arrow(arrowHead, flecthing, length);
+}
+
+ArrowHead GetArrowHeadType() {
+    Console.Write("Choose arrowhead type (steel, wood, obsidian) ");
+    string input = Console.ReadLine();
+
+    return input switch {
+        "steel" => ArrowHead.Steel,
+        "wood" => ArrowHead.Wood,
+        "obsidian" => ArrowHead.Obsidian
+    };
+}
+
+Fletching GetFletchingType() {
+    Console.Write("Choose fletching type (plastic, turkey feather, goose feather) ");
+    string input = Console.ReadLine();
+
+    return input switch {
+        "plastic" => Fletching.Plastic,
+        "turkey feather" => Fletching.TurkeyFeathers,
+        "goose feather" => Fletching.GooseFeathers
+    };
+}
+
+float GetLength() {
+    float length;
+
+    do {
+        Console.Write("Choose arrow length between 60 and 100: ");
+        length = Convert.ToSingle(Console.ReadLine());
+    } while (length < 60 || length > 100);
+
+    return length;
+}
+
