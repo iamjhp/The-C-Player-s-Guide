f9c113b667be7ae2bc19b15cef3f350d8b9639b7
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-23 02:21:30 +0100

    Finish 24 The Card

---
+++ 24-The-Card/TheCard/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCard {
+    public class Card {
+        public Color Color { get; }
+        public Rank Rank { get; }
+
+        public Card(Color color, Rank rank) {
+            Color = color;
+            Rank = rank;
+        }
+
+        public bool isSymbol => Rank == Rank.DollarSign || Rank == Rank.Percent || Rank == Rank.Caret || Rank == Rank.Ampersand;
+        public bool isNumber => !isSymbol;
+    }
+}
---
+++ 24-The-Card/TheCard/Enum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCard {
+    public enum Color {
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
+
+    public enum Rank {
+        One, 
+        Two, 
+        Three, 
+        Four, 
+        Five, 
+        Six, 
+        Seven, 
+        Eight, 
+        Nine, 
+        Ten, 
+        DollarSign, 
+        Percent, 
+        Caret, 
+        Ampersand
+    }
+}
