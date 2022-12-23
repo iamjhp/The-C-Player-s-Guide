862488cdd4eeefd359168317e585793dafb628d6
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-23 13:15:00 +0100

    Finish 24 The Password Validator

---
+++ 24-The-Password-Validator/ThePasswordValidator/PasswordValidator.cs
@@ -0,0 +1,47 @@
+﻿namespace ThePasswordValidator {
+    public class PasswordValidator {
+        public bool IsValid(string password) {
+            if (password.Length < 6) return false;
+            if (password.Length > 13) return false;
+            if (!HasUpperCase(password)) return false;
+            if (!HasLowerCase(password)) return false;
+            if (!HasDigits(password)) return false;
+            if (Contains(password, 'T')) return false;
+            if (Contains(password, '&')) return false;
+
+            return true;
+        }
+
+        private bool HasUpperCase(string password) {
+            foreach (char ch in password) {
+                if (char.IsUpper(ch))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasLowerCase(string password) {
+            foreach (char ch in password) {
+                if (char.IsLower(ch))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasDigits(string password) {
+            foreach (char ch in password) {
+                if (char.IsDigit(ch))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool Contains(string password, char letter) {
+            foreach (char ch in password) {
+                if (ch == letter)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 24-The-Password-Validator/ThePasswordValidator/Program.cs
@@ -0,0 +1,14 @@
+﻿using ThePasswordValidator;
+
+PasswordValidator validator = new PasswordValidator();
+
+while (true) {
+    Console.WriteLine("Enter a password:");
+    string? password = Console.ReadLine();
+
+    if (password == "" || password == null) break;
+
+    if (validator.IsValid(password)) Console.WriteLine("Your password is valid");
+    else Console.WriteLine("Invalid password");
+    Console.WriteLine("");
+}
