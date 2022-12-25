9a0a7ab21dc0a18ac9ea6eea4cf2f999732f6ece
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-12-26 00:59:26 +0100

    Finish The Old Robot

---
+++ 26-The-Old-Robot/TheOldRobot/OffCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOldRobot {
+    public class OffCommand : RobotCommand {
+        public override void Run(Robot robot) {
+            robot.IsPowered = false;
+        }
+    }
+}
---
+++ 26-The-Old-Robot/TheOldRobot/RobotCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOldRobot {
+    public abstract class RobotCommand {
+        public abstract void Run(Robot robot);
+    }
+}
