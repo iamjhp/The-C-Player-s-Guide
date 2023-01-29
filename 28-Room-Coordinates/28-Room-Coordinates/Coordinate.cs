eca805a3f206637025409f57e0175f40d55b000f
parkjoo1 <parkjoo1@students.zhaw.ch>
2023-01-29 14:19:01 +0100

    Finish 28

---
+++ 28-Room-Coordinates/28-Room-Coordinates/Coordinate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Room_Coordinates {
+    public struct Coordinate {
+
+        public int Row { get; }
+        public int Col { get; }
+
+        public Coordinate(int row, int col) {
+            Row = row;
+            Col = col;
+        }
+
+        public static Boolean IsAdjacent(Coordinate a, Coordinate b) {
+            return a.Row == b.Row || a.Col == b.Col;
+        }
+
+    }
+}
---
+++ 28-Room-Coordinates/28-Room-Coordinates/Program.cs
@@ -0,0 +1,9 @@
+﻿using _28_Room_Coordinates;
+
+Coordinate a = new Coordinate(3, 3);
+Coordinate b = new Coordinate(2, 3);
+Coordinate c = new Coordinate(2, 2);
+
+Console.WriteLine(Coordinate.IsAdjacent(a, b)); // Should be True
+Console.WriteLine(Coordinate.IsAdjacent(b, c)); // Should be True
+Console.WriteLine(Coordinate.IsAdjacent(a, c)); // Should be False
