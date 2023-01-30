093eb646efa369849d3a2060bea1b8dbbf168b34
parkjoo1 <parkjoo1@students.zhaw.ch>
2023-01-30 20:00:43 +0100

    Finish 34 Better Random

---
+++ 34-Better-Random/34-Better-Random/Program.cs
@@ -0,0 +1,8 @@
+﻿using _34_Better_Random;
+
+Random random = new Random();
+
+Console.WriteLine(random.NextDouble(100));
+Console.WriteLine(random.NextString("up", "down", "left", "right"));
+Console.WriteLine(random.CoinFlip());
+Console.WriteLine(random.CoinFlip(0.25));
---
+++ 34-Better-Random/34-Better-Random/RandomExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_Better_Random {
+    public static class RandomExtension {
+
+        public static double NextDouble(this Random random, double max) => random.NextDouble() * max;
+
+        public static string NextString(this Random random, params string[] options) => options[random.Next(options.Length)];
+
+        public static bool CoinFlip(this Random random, double probability = 0.5) => random.NextDouble() < probability;
+    }
+}
