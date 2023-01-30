15ed8c89fd70596d26bfcdc4a09a8e0b10fabff5
parkjoo1 <parkjoo1@students.zhaw.ch>
2023-01-30 15:33:18 +0100

    Finish 34 Safer Number Crunching

---
+++ 34-Safer-Number-Crunching/34-Safer-Number-Crunching/Program.cs
@@ -0,0 +1,18 @@
+﻿int intNumber;
+
+do {
+    Console.WriteLine("Enter an integer");
+} while (!int.TryParse(Console.ReadLine(), out intNumber));
+Console.WriteLine(intNumber);
+
+double doubleNumber;
+do {
+    Console.WriteLine("Enter a number");
+} while (!double.TryParse(Console.ReadLine(), out doubleNumber));
+Console.WriteLine(doubleNumber);
+
+bool enteredBool;
+do {
+    Console.WriteLine("Enter true or false");
+} while (!bool.TryParse(Console.ReadLine(), out enteredBool));
+Console.WriteLine(enteredBool);
