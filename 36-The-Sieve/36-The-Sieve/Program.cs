6ed3fe98e1a3480dd3870a8ec5d733050f54fc11
parkjoo1 <parkjoo1@students.zhaw.ch>
2023-01-31 12:56:08 +0100

    Finish 36 The Sieve

---
+++ 36-The-Sieve/36-The-Sieve/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Xml.Serialization;
+using _36_The_Sieve;
+
+Console.WriteLine("Choose 1=even, 2=positive or 3=multipleOfTen Filter");
+int input = int.Parse(Console.ReadLine());
+
+Sieve sieve = input switch {
+    1 => new Sieve(IsEven),
+    2 => new Sieve(IsPositive),
+    3 => new Sieve(IsMultiplesOfTen)
+};
+
+while (true) {
+    Console.WriteLine("Enter a number:");
+    int number = int.Parse(Console.ReadLine());
+
+    string goodOrBad = sieve.IsGood(number) ? "good" : "bad";
+    Console.WriteLine(goodOrBad);
+}
+
+bool IsEven(int number) => number % 2 == 0;
+bool IsPositive(int number) => number > 0;
+bool IsMultiplesOfTen(int number) => number % 10 == 0;
---
+++ 36-The-Sieve/36-The-Sieve/Sieve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_The_Sieve {
+    public class Sieve {
+        private Func<int, bool> decisionFunc;
+
+        public Sieve(Func<int, bool> decisionFunc) {
+            this.decisionFunc = decisionFunc;
+        }
+
+        public bool IsGood(int number) => this.decisionFunc(number);
+    }
+}
